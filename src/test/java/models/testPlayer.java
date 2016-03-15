c1b9c02295ee3cc64935d7deea9a5ef509333573
Jaydeep Rotithor <rotithoj@flip1.engr.oregonstate.edu>
2016-03-14 22:27:16 -0700

    tests

--- src/test/java/models/testPlayer.java
+++ src/test/java/models/testPlayer.java
@@ -2,6 +2,8 @@ package models;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNotEquals;
@@ -73,5 +75,136 @@ public class testPlayer {
         p.bet(25);
         p.blackjackBaby();
         assertNotEquals(p.money, p2.money);
+    }
+    @Test
+    public void testSplit(){
+        BlackJack g = new BlackJack();
+        ArrayList<Card> playerHand = new ArrayList<>();
+        ArrayList<Card> dealerHand = new ArrayList<>();
+        playerHand.add(new Card(7, Suit.Spades));
+        playerHand.add(new Card(7, Suit.Hearts));
+        g.player.hand = playerHand;
+        g.player.bet(5);
+        g.player.split();
+
+        assertEquals(10,g.player.playerBet);
+
+    }
+    @Test
+    public void testSplit2(){
+        BlackJack g = new BlackJack();
+        ArrayList<Card> playerHand = new ArrayList<>();
+        ArrayList<Card> dealerHand = new ArrayList<>();
+        playerHand.add(new Card(7, Suit.Spades));
+        playerHand.add(new Card(7, Suit.Hearts));
+        g.player.hand = playerHand;
+        g.player.bet(5);
+        g.player.split();
+
+        assertEquals(true,g.player.is_split);
+
+    }
+    @Test
+    public void testSplit3(){
+        BlackJack g = new BlackJack();
+        ArrayList<Card> playerHand = new ArrayList<>();
+        ArrayList<Card> dealerHand = new ArrayList<>();
+        playerHand.add(new Card(7, Suit.Spades));
+        playerHand.add(new Card(7, Suit.Hearts));
+        g.player.hand = playerHand;
+        g.player.bet(5);
+        g.player.split();
+
+        assertEquals(1,g.player.split_hand.size());
+
+    }
+    @Test
+    public void testSplit4(){
+        BlackJack g = new BlackJack();
+        ArrayList<Card> playerHand = new ArrayList<>();
+        ArrayList<Card> dealerHand = new ArrayList<>();
+        playerHand.add(new Card(7, Suit.Spades));
+        playerHand.add(new Card(7, Suit.Hearts));
+        g.player.hand = playerHand;
+        g.player.bet(5);
+        g.player.split();
+
+        assertEquals(1,g.player.hand.size());
+
+    }
+    @Test
+    public void testSplitHit(){
+        BlackJack g = new BlackJack();
+        ArrayList<Card> playerHand = new ArrayList<>();
+        ArrayList<Card> dealerHand = new ArrayList<>();
+        playerHand.add(new Card(7, Suit.Spades));
+        playerHand.add(new Card(7, Suit.Hearts));
+        g.player.hand = playerHand;
+        g.player.bet(5);
+        g.player.split();
+        g.player.split_hit(playerHand);
+        assertEquals(1,playerHand.size());
+
+    }
+    @Test
+    public void testSplitHit2(){
+        BlackJack g = new BlackJack();
+        ArrayList<Card> playerHand = new ArrayList<>();
+        ArrayList<Card> dealerHand = new ArrayList<>();
+        playerHand.add(new Card(7, Suit.Spades));
+        playerHand.add(new Card(7, Suit.Hearts));
+
+        g.player.hand = playerHand;
+        g.player.bet(5);
+        g.player.split();
+        ArrayList<Card> playerSplitHand = new ArrayList<>();
+        playerHand.add(new Card(9, Suit.Hearts));
+        playerHand.add(new Card(9, Suit.Hearts));
+        playerSplitHand.add(new Card(9, Suit.Hearts));
+        playerSplitHand.add(new Card(9, Suit.Hearts));
+        playerSplitHand.add(new Card(9, Suit.Hearts));
+        playerSplitHand.add(new Card(9, Suit.Hearts));
+        g.player.split_hand = playerSplitHand;
+        g.player.split_hit(playerHand);
+        assertEquals(true, g.player.turnEnd);
+
+    }
+    @Test
+    public void testSplitHit3(){
+        BlackJack g = new BlackJack();
+        ArrayList<Card> playerHand = new ArrayList<>();
+        ArrayList<Card> dealerHand = new ArrayList<>();
+        playerHand.add(new Card(7, Suit.Spades));
+        playerHand.add(new Card(7, Suit.Hearts));
+
+        g.player.hand = playerHand;
+        g.player.bet(5);
+        g.player.split();
+        ArrayList<Card> playerSplitHand = new ArrayList<>();
+        playerHand.add(new Card(9, Suit.Hearts));
+        playerHand.add(new Card(9, Suit.Hearts));
+        playerSplitHand.add(new Card(1, Suit.Hearts));
+        playerSplitHand.add(new Card(1, Suit.Hearts));
+        playerSplitHand.add(new Card(9, Suit.Hearts));
+        playerSplitHand.add(new Card(9, Suit.Hearts));
+        g.player.split_hand = playerSplitHand;
+        g.player.split_hit(playerHand);
+        assertEquals(true, g.player.turnEnd);
+
+    }
+    @Test
+    public void testPlay() {
+        BlackJack g = new BlackJack();
+        ArrayList<Card> deck = new ArrayList<>();
+        ArrayList<Card> dealerHand = new ArrayList<>();
+        deck.add(new Card(13, Suit.Clubs));
+        deck.add(new Card(11, Suit.Spades));
+        deck.add(new Card(1, Suit.Clubs));
+        deck.add(new Card(5, Suit.Hearts));
+        dealerHand.add(new Card(4, Suit.Clubs));
+        dealerHand.add(new Card(3, Suit.Diamonds));
+        g.dealer.hand = dealerHand;
+        g.dealer.play(deck);
+        assertEquals(23, g.player.getSplitTotal());
     }
 }
